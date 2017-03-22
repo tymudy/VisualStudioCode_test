0f348f40ef95a711b0033ff4cc1232b7b90a5493
Emma Timea Sorban <emma.sorban@fortech.ro>
2017-03-22 14:24:01 +0200

    redone

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+'use strict'; //JS code executes in strict mode, undeclared variable usages causes error
+
+module.exports = function (grunt) {
+
+    //config for all tasks
+    grunt.initConfig({
+
+    });
+
+};
